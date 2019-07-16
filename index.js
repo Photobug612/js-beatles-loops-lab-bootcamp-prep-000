f28d9687adf4cd06f4ae9d5b9b850e7e40fb8787
Arlene George <aemg@live.com>
2019-07-16 03:25:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, insutruments) {
   var allMusicians = [];
   for (var i=0; i < musicians.length; i++) {
-    allMusicians.push(musicians[i] + "plays" + insturments[i])
+    allMusicians.push(musicians[i] + "plays" + insutruments[i])
   }
   return allMusicians
 }
